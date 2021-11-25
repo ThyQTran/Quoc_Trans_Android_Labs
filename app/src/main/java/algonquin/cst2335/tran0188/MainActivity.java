bb2242c4d736113c708bab0055eb4c9c25b36f57
ThyQTran <83242773+ThyQTran@users.noreply.github.com>
2021-11-24 23:27:01 -0800

    Finished Week9 in JSON

--- app/src/main/java/algonquin/cst2335/tran0188/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/tran0188/MainActivity.java
@@ -1,14 +1,42 @@
 package algonquin.cst2335.tran0188;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 /**
  * Page that authenticates that a submitted password has an uppercase character, a lowercase
@@ -23,30 +51,148 @@ public class MainActivity extends AppCompatActivity {
     /** This variable holds text at the centre of the screen*/
     private TextView tv = null;
 
-    /** This variable holds the text that is entered in the password field*/
-    private EditText et = null;
+    /** This variable holds the text that is entered in the city field where we would like to search for the forecast*/
+    private EditText cityText = null;
 
-    /** This variable holds the button that when clicked submits the password*/
-    private Button btn = null;
+    /** This variable holds the button that when clicked submits the city we want the forcast for*/
+    private Button forecastBtn = null;
 
+    private String stringURL;
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+        forecastBtn = findViewById(R.id.forecastButton);
+        cityText = findViewById(R.id.cityTextField);
+
+
+
         tv = findViewById(R.id.textView);
-        et = findViewById(R.id.editText);
-        btn = findViewById(R.id.button);
+//w6        et = findViewById(R.id.editText);
+//w6        btn = findViewById(R.id.button);
 
-        btn.setOnClickListener(clk ->{
-            String password = et.getText().toString();
+        forecastBtn.setOnClickListener(clk ->{
 
-            if(checkPasswordComplexity(password)){
-                tv.setText("Your password meets the requirements");
-            }else{
-                tv.setText("You shall not pass!");
-            };
+            Executor newThread = Executors.newSingleThreadExecutor();
+
+            newThread.execute( () -> {
+                /* This runs in a separate thread */
+                try {
+                    String cityName = cityText.getText().toString();
+
+                    stringURL = "https://api.openweathermap.org/data/2.5/weather?q=" + URLEncoder.encode(cityName,"UTF-8") + "&appid=bdd1b06f072a3498fc5ca8d9a3e87c1d&units=metric";
+
+                    URL url = new URL(stringURL); //Builds the server connection  as the URL passed here is the server you want to connect to
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection(); //Connects to the server
+                    InputStream in = new BufferedInputStream(urlConnection.getInputStream()); //Waits for a response from the server
+
+                    String text = (new BufferedReader( //Converts the inputStream from the server into Java String object to read JSON name/value pair data in Android
+                            new InputStreamReader(in, StandardCharsets.UTF_8)))
+                            .lines()
+                            .collect(Collectors.joining("\n"));
+
+                    int i = 0; //Allows for the test variable to run fully and value of variable to display in debugging console
+
+                    JSONObject theDocument = new JSONObject(text);
+
+                    JSONArray weatherArray = theDocument.getJSONArray("weather");
+                    JSONObject position0 = weatherArray.getJSONObject(0);
+                    String description = position0.getString("description");
+                    String iconName = position0.getString("icon");
+
+                    JSONObject mainObject = theDocument.getJSONObject( "main" );
+
+                    double current = mainObject.getDouble("temp");
+                    double min = mainObject.getDouble("temp_min");
+                    double max = mainObject.getDouble("temp_max");
+                    int humidity = mainObject.getInt("humidity");
+
+                    //Downloads URL picture and stores it as a bitmap
+                    Bitmap image = null;
+
+
+                    File file = new File(getFilesDir(), iconName + ".png");
+                    if(file.exists()){
+                        image = BitmapFactory.decodeFile(getFilesDir()+"/" + iconName + ".png");
+                    }else {
+                        URL imgUrl = new URL("https://openweathermap.org/img/w/" + iconName + ".png");
+                        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                        connection.connect();
+                        int responseCode = connection.getResponseCode();
+                        if (responseCode == 200) {
+
+                            image = BitmapFactory.decodeStream(connection.getInputStream());
+                            image.compress(Bitmap.CompressFormat.PNG, 100, openFileOutput(iconName+".png", Activity.MODE_PRIVATE));
+                        }
+                    }
+
+                    ImageView iv = findViewById(R.id.icon);
+                    iv.setImageBitmap(image);
+
+
+                    runOnUiThread(() -> {
+                        TextView tv = findViewById(R.id.temp);
+                        tv.setText("The current temperature is " + current);
+                        tv.setVisibility(View.VISIBLE);
+
+                        tv = findViewById(R.id.minTemp);
+                        tv.setText("The min temperature is " + current);
+                        tv.setVisibility(View.VISIBLE);
+
+                        tv = findViewById(R.id.maxTemp);
+                        tv.setText("The max temperature is " + current);
+                        tv.setVisibility(View.VISIBLE);
+
+                        tv = findViewById(R.id.humidity);
+                        tv.setText("The humidity is " + humidity + "%");
+                        tv.setVisibility(View.VISIBLE);
+
+                        tv = findViewById(R.id.description);
+                        tv.setText(description);
+                        tv.setVisibility(View.VISIBLE);
+
+ //                           ImageView iv = findViewById(R.id.icon);
+ //                           iv.setImageBitmap(image);
+                        iv.setVisibility(View.VISIBLE);
+                    });
+
+
+
+                        //Saves icon to the device so that next time it doesn't have to be downloaded again
+                        FileOutputStream fOut = null;
+                        try {
+                            fOut = openFileOutput(iconName + ".png", Context.MODE_PRIVATE);
+ //                           image.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+                            fOut.flush();
+                            fOut.close();
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException ioe) {
+                    Log.e("Connection error: ", ioe.getMessage());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } );
+
+
+
+
+
+//w6            if(checkPasswordComplexity(password)){
+//w6                tv.setText("Your password meets the requirements");
+//w6            }else{
+//w6                tv.setText("You shall not pass!");
         });
+
 
     }
 
